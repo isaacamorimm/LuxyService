459556b9624741d577fde75729991928426199eb
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-02-17 20:57:33 -0300

    fix: sessão "serviços" começa a partir do início

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { Services } from './pages/Services/Services';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
