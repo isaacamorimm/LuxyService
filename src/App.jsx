ad5cffa88512823ac29f2d782cd14b71af0302c8
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-02-17 12:29:24 -0300

    style: icon adicionado na janela e footer mais legível

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
-import { Services } from './pages/Services/Services'; /* <-- A correção está aqui! */
+import { Services } from './pages/Services/Services';
 
 function App() {
   return (
