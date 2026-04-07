240e599004c04327bfad235b7388a87e99e8f755
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-04-07 09:45:00 -0300

    feat: add google site verification meta tag and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import { fileURLToPath } from "url"
+import Sitemap from "vite-plugin-sitemap"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sitemap({hostname: "https://luxyservice.com.br"})],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
